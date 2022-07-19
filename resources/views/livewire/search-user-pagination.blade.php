9f2d97d3b51b8593b18aea36ba0f652c9f5b9a17
wiambndj <wiambndj97@gmail.com>
2022-07-19 09:29:57 +0200

    correction erreur au niveau dune variable

--- resources/views/livewire/search-user-pagination.blade.php
+++ resources/views/livewire/search-user-pagination.blade.php
@@ -254,10 +254,10 @@
                        $now = Carbon\Carbon::now();
                        ?>
                        <h6 style="color:red;">( {{$row->date_limite}} )</h6>
-                            @if($date_limite < $now && user->paye == 1)
+                            @if($date_limite < $now && $row->paye == 1)
                        <span class="badge badge-warning"> dépassé</span>
                             @endif
-                            @if(user->paye == 1)
+                            @if($row->paye == 1)
                             <span class="badge badge-info"> autorisé</span>
                             @endif
                        @else
